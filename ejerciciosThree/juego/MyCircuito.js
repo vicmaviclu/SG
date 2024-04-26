1f79db35f16a126c7a200aab745f1d1f1774745e
Casalini <vgcasalini@correo.ugr.es>
2024-04-26 15:47:16 +0200

    circuito archivo

---
+++ ejerciciosThree/juego/MyCircuito.js
@@ -0,0 +1,20 @@
+import * as THREE from '../libs/three.module.js'
+ 
+class Juego extends THREE.Object3D {
+  constructor(gui,titleGui) {
+    super();
+    this.createGUI(gui,titleGui);
+
+    
+  }
+  
+  createGUI (gui,titleGui) {
+
+  }
+
+  update () {
+
+  }
+}
+
+export { Juego };

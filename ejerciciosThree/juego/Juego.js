091c3e74ed6cdc9d8dee925547d80702a928e89d
Casalini <vgcasalini@correo.ugr.es>
2024-04-26 15:51:15 +0200

    juego

--- ejerciciosThree/juego/Juego.js
+++ ejerciciosThree/juego/Juego.js
@@ -3,8 +3,6 @@ import * as THREE from '../libs/three.module.js'
 class Juego extends THREE.Object3D {
   constructor(gui,titleGui) {
     super();
-    // Se crea la parte de la interfaz que corresponde a la caja
-    // Se crea primero porque otros métodos usan las variables que se definen para la interfaz
     this.createGUI(gui,titleGui);
 
   }

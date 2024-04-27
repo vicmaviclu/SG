b61ff63468e5d2a44966dd01b1153698e890b248
Joaquin <joaquinsalas@correo.ugr.es>
2024-04-27 19:50:50 +0200

    hecho modelo del ovni por revolucion

--- ejerciciosThree/juego/MyOvni.js
+++ ejerciciosThree/juego/MyOvni.js
@@ -8,17 +8,45 @@ class MyOvni extends THREE.Object3D {
   constructor() {
     super();
     
-    var materialLoader = new MTLLoader();
-    var objectLoader = new OBJLoader();
-    materialLoader.load('../models/ovni/ufo.mtl', 
-        (materials) => {
-            objectLoader.setMaterials(materials);
-            objectLoader.load('../models/ovni/ufo.obj',
-                (object) => {
-                    this.add(object);
-                }, null, null);
-        });
-    
+    // var materialLoader = new MTLLoader();
+    // var objectLoader = new OBJLoader();
+    // materialLoader.load('../models/ovni/ufo.mtl', 
+    //     (materials) => {
+    //         objectLoader.setMaterials(materials);
+    //         objectLoader.load('../models/ovni/ufo.obj',
+    //             (object) => {
+    //                 this.add(object);
+    //             }, null, null);
+    //     }
+    // );
+    // this.position.x = 0;
+    // this.position.y = -10;
+    // this.position.z = 0;
+
+    /* MATERIAL */
+    var material = new THREE.MeshNormalMaterial();
+    material.flatShading = true;
+    material.needsUpdate = true;
+
+    /* SHAPE */
+    var shape = new THREE.Shape();
+    shape.moveTo(0, -0.8);
+    shape.quadraticCurveTo(1.7, -0.8, 1.7, 0);
+    shape.lineTo(2, 0);
+    shape.quadraticCurveTo(2, 0.9, 1.1, 0.9);
+    shape.lineTo(1.1, 1);
+    shape.lineTo(1, 1);
+    shape.quadraticCurveTo(1, 2, 0, 2);
+
+    /* GEOMETRY */
+    var points = shape.getPoints();
+    var geometry = new THREE.LatheGeometry(points, 32);
+
+    /* MESH */
+    var mesh = new THREE.Mesh(geometry, material);
+
+    /* THIS */
+    this.add(mesh);    
   }
   
   update () {
